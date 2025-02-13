1048dc295b83d8a13dd590631f60605b91594bc6
Hedegaard9 <andreashedegaard99@gmail.com>
2025-02-13 14:01:26 +0100

    Fixet c++ del 2

--- ewma.cpp
+++ ewma.cpp
@@ -1,36 +1,50 @@
-#include <Rcpp.h>
-using namespace Rcpp;
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <cmath>
+#include <vector>
 
-// [[Rcpp::export]]
-NumericVector ewma_c(NumericVector x, double lambda, int start) {
-  int n = x.size();
-  int na = 0;
-  double initial_value = 0;
-  NumericVector var_vec(n, NA_REAL);
+namespace py = pybind11;
 
-  if(n <= start) {
-    return var_vec;
-  }
+// EWMA-funktion
+py::array_t<double> ewma_c(py::array_t<double> x, double lambda, int start) {
+    auto buf = x.request();
+    double* ptr = static_cast<double*>(buf.ptr);
+    int n = buf.shape[0];
 
-  // Calculate simple variance to get started (assuming mean = 0)
-  for(int i = 0; i < start; ++i) {
-    if(NumericVector::is_na(x(i))) {
-      na += 1;
-    } else {
-      initial_value += pow(x(i), 2);
+    std::vector<double> var_vec(n, NAN);
+    int na = 0;
+    double initial_value = 0;
+
+    if (n <= start) {
+        return py::array_t<double>(var_vec.size(), var_vec.data());
     }
-  }
-  initial_value = initial_value / (start - 1 - na);
 
-  // Iteratively Updating EWMA Vol
-  var_vec(start) = initial_value;
-  for(int j = start + 1; j < n; ++j) {
-    if(NumericVector::is_na(x(j - 1))) {
-      var_vec(j) = var_vec(j - 1);
-    } else {
-      var_vec(j) = lambda * var_vec(j - 1) + (1 - lambda) * pow(x(j - 1), 2);
+    for (int i = 0; i < start; ++i) {
+        if (std::isnan(ptr[i])) {
+            na += 1;
+        } else {
+            initial_value += std::pow(ptr[i], 2);
+        }
     }
-  }
+    initial_value /= (start - 1 - na);
 
-  return sqrt(var_vec);
+    var_vec[start] = initial_value;
+    for (int j = start + 1; j < n; ++j) {
+        if (std::isnan(ptr[j - 1])) {
+            var_vec[j] = var_vec[j - 1];
+        } else {
+            var_vec[j] = lambda * var_vec[j - 1] + (1 - lambda) * std::pow(ptr[j - 1], 2);
+        }
+    }
+
+    for (auto &v : var_vec) {
+        v = std::sqrt(v);
+    }
+
+    return py::array_t<double>(var_vec.size(), var_vec.data());
+}
+
+// Eksporter funktion til Python
+PYBIND11_MODULE(ewma, m) {
+    m.def("ewma_c", &ewma_c, "EWMA Volatility Calculation");
 }
