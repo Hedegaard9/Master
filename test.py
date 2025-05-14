983265a6f05bcbf3d0aa1516e46c3cd29564f772
JMOELLER25 <167001922+JMOELLER25@users.noreply.github.com>
2025-05-14 10:33:49 +0200

    Diverse updates

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import ewma
-
-data = np.array([0.1, 0.2, 0.3, 0.4, np.nan, 0.5, 0.6])
-lambda_val = 0.94
-start = 3
-
-result = ewma.ewma_c(data, lambda_val, start)
-print(result)
